35bd289d987d8f209700bb1917cb3506ccff6eea
contehboy <133304258+contehboy@users.noreply.github.com>
2023-10-04 15:49:10 -0500

    getdb implemented

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -24,6 +24,12 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const req = store.getAll();
+  const result = await req;
+};
 
 initdb();
